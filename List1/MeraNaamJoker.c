e04e544c4cabbe50708d36e3f5eb7fa23398f4a9
siddhesh <sid@iiitb>
2026-09-17 13:56:56 +0000

    first commit

---
+++ List1/MeraNaamJoker.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+	
+	printf("My Name Siddhesh \n");
+	return 0;
+	
+}
---
+++ List1/pokemon.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+	printf("Pika pika Pikachuuuuuu \n");
+	return 0;
+}
